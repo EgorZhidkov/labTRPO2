ffe1cca82bc7dc57cf97b0e8b542cc53bd196fc3
Rudzinsky Viacheslav <slava_99@list.ru>
2022-10-05 14:32:23 +0300

    Git commit by Slava Signed-off-by: Slava <slava_99@list.ru>

--- main.c
+++ main.c
@@ -5,5 +5,7 @@ int main(int argc, char **argv)
     char first[255], last[255];
     askname(first, last);
     printf("Hello, %s %s!\n", first, last);
+    printf("Hello, I'm Naruto!!! "\n);
+    
     return 0;
 }
